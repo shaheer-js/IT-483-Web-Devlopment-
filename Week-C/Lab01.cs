61ac774cb6798e6f6b75d23c3fcad8ecca633eb6
shaheer-js <57514163+shaheer-js@users.noreply.github.com>
2020-04-24 23:36:46 -0500

    Lab01 files added

---
+++ Week-C/Lab01.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Week_C
+{
+    public class Lab01
+    {
+        public Lab01()
+        {
+            Console.WriteLine("CS 201 Restaurant Guide\n");
+            String response;
+            char f;
+            char s;
+            bool fancy;
+            bool spicy;
+            Console.WriteLine("Do you like spicy food?(y/n):");
+            response = Console.ReadLine();
+            s = response[0];//Taking only the first character of the string
+            if ((s == 'y') || (s == 'Y'))
+                spicy = true;
+            else
+                spicy = false;
+
+            Console.Write("Do you want to go to a fancy restaurant?(y/n):");
+            response = Console.ReadLine();
+            f = response[0];
+            fancy = ((f == 'y') || (f == 'Y'));
+            if (spicy)
+            {     //First Block of Input choices
+                if (fancy)
+                    Console.WriteLine("I suggest you go to Thai Garden Palace.");
+                else
+                    Console.WriteLine("I suggest you to go to Alberto's Tacqueria.");
+            }
+            else
+            {     //Second Block of Input choices
+                if (fancy)
+                    Console.WriteLine("I suggest you to go to Chez Paris.");
+                else
+                    Console.WriteLine("I suggest you go to Joe's Diner.");
+            }
+        }
+    }
+}
+
+
---
+++ Week-C/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Week_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Lab01 lab = new Lab01();
+        }
+    }
+}
+
